0fc838681168a35f5597a9f510b22ba32d971f92
Gary <bernie71022@gmail.com>
2025-03-16 22:58:16 +0800

    add test.c

--- test.c
+++ test.c
@@ -1,7 +1,28 @@
-#include "test_header.h"
+// Online C compiler to run C program online
+/*
+請設計一段程式碼輸出如下:
+    1
+   121
+  12321
+ 1234321
+123454321
+*/
 #include <stdio.h>
 
-int main(){
 
-    test();
+int main()
+{         
+    int input = 0;
+    scanf("%d", &input);
+    for(int i=1; i<=input; i++){
+        for(int j=input-i; j>0; j--){
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++){
+            printf("%d", j);
+        }
+        for(int j=i-1; j>0; j--)printf("%d", j);
+        printf("\n");
+    }
 }
+              
